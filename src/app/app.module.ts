b6dde3c552171eff841cde3978bab95fcefd00bd
Luiz Henrique A. Mello <lhamello@gmail.com>
2020-07-22 00:18:09 -0300

    criado primeiro módulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PhotoComponent } from './photo/photo/photo.component';
+import { PhotosModule } from './photos/photos.module';
 
 @NgModule({
-  declarations: [AppComponent, PhotoComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
-  bootstrap: [AppComponent],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    PhotosModule
+  ],
+  providers: [
+
+  ],
+  bootstrap: [
+    AppComponent
+  ],
 })
 export class AppModule { }
---
+++ src/app/photos/photos.module.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { PhotoComponent } from './photo/photo.component';
+
+@NgModule({
+  declarations: [
+    PhotoComponent
+  ],
+  exports: [
+    PhotoComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PhotosModule { }
