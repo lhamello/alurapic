63afc842f1d578deae05b1da9595732722adbaf7
Luiz Henrique A. Mello <lhamello@gmail.com>
2020-07-21 23:39:10 -0300

    aidicionado bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'alurapic';
+  description = 'Grêmio';
+  url = 'https://www.gazetaesportiva.com/wp-content/uploads/imagem/2017/12/01/38735664791_f625e06c5b_k-1024x683.jpg';
 }
