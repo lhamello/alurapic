3170c017ee4f7a5104287c721df80aed6cedaf0e
Luiz Henrique A. Mello <lhamello@gmail.com>
2020-07-25 00:14:41 -0300

    rota parametrida

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,5 +1,6 @@
 import { PhotoService } from './../photo/photo.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-photo-list',
@@ -10,9 +11,14 @@ export class PhotoListComponent implements OnInit {
 
   photos: Object[] = [];
 
-  constructor(private photoService: PhotoService) { }
+  constructor(
+    private photoService: PhotoService,
+    private activateRoute: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    const userName = this.activateRoute.snapshot.params.userName;
+
     this.photoService
       .listFromUser('flavio')
       .subscribe(photos => this.photos = photos);
