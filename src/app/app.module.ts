468792732f3a0a3e8bd01e37e6fd974368eade84
Luiz Henrique A. Mello <lhamello@gmail.com>
2020-07-26 08:52:15 -0300

    corrigido import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ErrorsModule } from './errors/errors.module';
 import { PhotosModule } from './photos/photos.module';
+
 
 @NgModule({
   declarations: [
