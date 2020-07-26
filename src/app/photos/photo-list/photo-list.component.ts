3eff2f9a238b708a0037f3fc6367a0a75601bc10
Luiz Henrique A. Mello <lhamello@gmail.com>
2020-07-26 09:05:22 -0300

    alteração de formatação

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,9 +1,9 @@
-import { PhotoService } from './../photo/photo.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
 import { Photo } from '../photo/photo';
+import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'ap-photo-list',
@@ -20,13 +20,13 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   userName: string = '';
 
   constructor(
-    private activateRoute: ActivatedRoute,
+    private activatedRoute: ActivatedRoute,
     private photoService: PhotoService
   ) { }
 
   ngOnInit(): void {
-    this.userName = this.activateRoute.snapshot.params.userName;
-    this.photos = this.activateRoute.snapshot.data['photos'];
+    this.userName = this.activatedRoute.snapshot.params.userName;
+    this.photos = this.activatedRoute.snapshot.data['photos'];
     this.debounce
       .pipe(debounceTime(300))
       .subscribe(filter => this.filter = filter);
@@ -40,11 +40,8 @@ export class PhotoListComponent implements OnInit, OnDestroy {
     this.photoService
       .listFromUserPaginated(this.userName, ++this.currentPage)
       .subscribe(photos => {
-        this.photos.concat(photos);
-
-        if (!photos.length) {
-          this.hasMore = false;
-        }
+        this.photos = this.photos.concat(photos);
+        if(!photos.length) this.hasMore = false;
       });
   }
 }
