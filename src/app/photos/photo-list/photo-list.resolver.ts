601193655c9c090c47f631ef7ca98ba401ca03fd
Luiz Henrique A. Mello <lhamello@gmail.com>
2020-07-26 09:01:04 -0300

    correção de import

--- src/app/photos/photo-list/photo-list.resolver.ts
+++ src/app/photos/photo-list/photo-list.resolver.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Photo } from '../photo/photo';
-import { PhotoService } from './../photo/photo.service';
+import { PhotoService } from '../photo/photo.service';
 
 @Injectable({ providedIn: 'root' })
 export class PhotoListResolver implements Resolve<Observable<Photo[]>> {
