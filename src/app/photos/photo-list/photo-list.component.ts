5e8754d04b9d160257320f70812ccf9d33c6c8f6
Luiz Henrique A. Mello <lhamello@gmail.com>
2020-07-26 08:41:32 -0300

    melhorando a experiência do usuário

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { PhotoService } from './../photo/photo.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 import { Photo } from '../photo/photo';
 
 @Component({
@@ -8,20 +10,41 @@ import { Photo } from '../photo/photo';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
   photos: Photo[] = [];
+  filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
+  hasMore: boolean = true;
+  currentPage: number = 1;
+  userName: string = '';
 
   constructor(
-    private photoService: PhotoService,
-    private activateRoute: ActivatedRoute
+    private activateRoute: ActivatedRoute,
+    private photoService: PhotoService
   ) { }
 
   ngOnInit(): void {
-    const userName = this.activateRoute.snapshot.params.userName;
+    this.userName = this.activateRoute.snapshot.params.userName;
+    this.photos = this.activateRoute.snapshot.data['photos'];
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe(filter => this.filter = filter);
+  }
 
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
+  }
+
+  load() {
     this.photoService
-      .listFromUser(userName)
-      .subscribe(photos => this.photos = photos);
+      .listFromUserPaginated(this.userName, ++this.currentPage)
+      .subscribe(photos => {
+        this.photos.concat(photos);
+
+        if (!photos.length) {
+          this.hasMore = false;
+        }
+      });
   }
 }
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
+import { FilterByDescriptionPipe } from './photo-list/filter-by-description.pipe';
+import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotosComponent } from './photo-list/photos/photos.component';
 import { PhotoComponent } from './photo/photo.component';
@@ -9,10 +11,12 @@ import { PhotoComponent } from './photo/photo.component';
 
 @NgModule({
   declarations: [
+    FilterByDescriptionPipe,
+    LoadButtonComponent,
     PhotoComponent,
     PhotoListComponent,
     PhotoFormComponent,
-    PhotosComponent
+    PhotosComponent,
   ],
   imports: [
     CommonModule,
