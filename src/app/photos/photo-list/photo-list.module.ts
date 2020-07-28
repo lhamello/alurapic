72d4756a8af17a4185589cb7785d7d590bacc654
Luiz Henrique A. Mello <lhamello@gmail.com>
2020-07-28 00:28:13 -0300

    instalado font-awesome

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -1,9 +1,9 @@
-import { PhotoModule } from './photos/photo.module';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FilterByDescription } from './filter-by-description.pipe';
 import { LoadButtonComponent } from './load-button/load-button.component';
 import { PhotoListComponent } from './photo-list.component';
+import { PhotoModule } from './photos/photo.module';
 import { PhotosComponent } from './photos/photos.component';
 
 @NgModule({
